d8dd21f8bed2ee9e2bfa6b9449041d4f8cb33087
kamal <kamalkj9368@gmail.com>
2024-03-30 09:31:26 +0530

    resoling

--- client/src/customComponents/NavigationBar.jsx
+++ client/src/customComponents/NavigationBar.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
-
+import { IoMdContact } from "react-icons/io";
+import { Button } from '@/components/ui/button';
 const NavigationBar = () => {
   return (
     <>
-      <Sheet>
+      <header>
+        <nav className="h-20 w-full bg-[#131517] flex flex-row justify-between items-center text-gray-500 px-6 ">
+          <div className="lg:w-1/3 hover:text-rose-500 text-4xl font-roboto font-semibold text-rose-500">
+            f-ai
+          </div>
+          <div className="hidden  lg:flex flex-row justify-between items-center gap-3 lg:gap-10">
+            <div className="hover:text-rose-600">Home</div>
+            <div className="hover:text-rose-600">Vision</div>
+            <div className="hover:text-rose-600">Solutions</div>
+            <div className="hover:text-rose-600">Plans and prices</div>
+            <div className="hover:text-rose-600 flex flex-row gap-1">
+              <div className="text-2xl">
+                <IoMdContact />
+              </div>
+              <div>login</div>
+            </div>
+            <div>
+              <Button variant="outline">get started</Button>
+            </div>
+            <div className="hover:text-rose-600">icon</div>
+          </div>
+          <div className='md:hidden lg:hidden'>
+          <Sheet>
             <SheetTrigger>
               <Button variant="link">
                 <div className="lg:hidden flex flex-col justify-center content-center  ">
@@ -28,6 +51,11 @@ const NavigationBar = () => {
               </div>
             </SheetContent>
           </Sheet>
+          </div>
+
+        </nav>
+      </header>
+
     </>
   )
 }
