60fe62b69ea1917d50753eecc0de3bbb1c5fe46e
kamal <kamalkj9368@gmail.com>
2024-03-31 10:59:36 +0530

    merging all files

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+<<<<<<< HEAD
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+=======
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+  <App></App>
+  </BrowserRouter>
+);
+>>>>>>> 887ff91 (merging all files)
