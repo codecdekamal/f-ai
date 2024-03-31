31727ee10707a3ad8a4a3f628b4f61d1b546333e
kamal <kamalkj9368@gmail.com>
2024-03-31 11:42:16 +0530

    checking

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,14 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-<<<<<<< HEAD
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
-=======
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 const root = createRoot(document.getElementById("root"));
@@ -18,4 +10,3 @@ root.render(
   <App></App>
   </BrowserRouter>
 );
->>>>>>> 887ff91 (merging all files)
