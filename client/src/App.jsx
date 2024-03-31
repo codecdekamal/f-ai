dfd44ac4371e7517e44e429cd246119ff77b78fd
kamal <kamalkj9368@gmail.com>
2024-03-31 12:53:05 +0530

    adding auth0

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/signin" element={<SigninPage/>}></Route>
-        <Route path="/signup" element={<SignUp/>}> </Route>
+        {/* <Route path="/signup" element={<SignUp/>}> </Route> */}
       </Routes>
     </>
   );
