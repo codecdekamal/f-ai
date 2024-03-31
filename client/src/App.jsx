ecf2a149187890c81a208ede56b7f6a3db41d8e6
kamal <kamalkj9368@gmail.com>
2024-03-31 12:28:38 +0530

    routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
 import "./App.css";
 import HomePage from "./pages/HomePage";
 import { Routes,Route } from "react-router-dom";
+import SignUp from "./customComponents/Signup";
+import SigninPage from "./pages/SigninPage";
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/signin" element={<SigninPage/>}></Route>
+        <Route path="signup" element={<SignUp/>}> </Route>
       </Routes>
     </>
   );
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,11 +1,8 @@
 import React, { useState } from "react";
-import { Button } from "@/components/ui/button";
-import MainCard from "@/customComponents/MainCard";
 import NavigationBar from "@/customComponents/NavigationBar";
 import Description from "@/customComponents/DescriptionOne";
 import DescriptionTwo from "@/customComponents/DescriptionTwo";
 import DescriptionThree from "@/customComponents/DescriptionThree";
-import Layout from "@/customComponents/Layout";
 import CostDescription from "@/customComponents/CostDescription";
 import DescriptionFour from "@/customComponents/DescriptionFour";
 import Footer from "@/customComponents/Footer";
