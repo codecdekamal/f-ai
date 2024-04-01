16a410d9bf0e9cadc7aeb747b4d518aeac6a18c7
kamal <kamalkj9368@gmail.com>
2024-04-01 18:39:32 +0530

    responsive

--- client/src/customComponents/DescriptionOne.jsx
+++ client/src/customComponents/DescriptionOne.jsx
@@ -1,31 +1,22 @@
-import React from "react";
-import { Button } from "@/components/ui/button";
-import { motion } from "framer-motion";
+import React from 'react'
+import { Button } from '@/components/ui/button'
+import {motion} from "framer-motion"
 const Description = () => {
   return (
     <>
-      <main className="h-[100vh] w-screen bg-black/90 grid grid-cols-6 ">
+        <main  className="h-[60vh] md:h-screen w-screen bg-black/90 grid  md:grid-cols-6 mt-20 ">
         <div className="col-span-4 ">
-          <div className="w-full h-full  flex flex-col justify-between py-5 pl-12">
-            <h1 className="font-bold font-monospace text-6xl text-rose-500">
+          <div className="w-full h-full flex flex-col justify-between  md:py-5 md:pl-12 px-10">
+            <h1 className="font-semibold font-monospace text-5xl text-rose-500 pt-10">
               f-ai
             </h1>
-            <motion.div
-              initial={{ x: -300, opacity: 0, scale: 0.4 }}
-              animate={{ opacity: 1, x: 0, scale: 1 }}
-              transition={{ duration: 1.5, delay: 1 }}
-            >
-              <p className="tracking-wide lg:leading-loose font-bold text-4xl text-white/70">
+            <motion.div initial={{x:-300,opacity:0,scale:0.4}} animate={{opacity:1,x:0,scale:1}} transition={{duration:1.5,delay:1}} > 
+              <p className="tracking-wide lg:leading-loose font-bold text-2xl md:text-4xl text-white/70">
                 Next-Gen solution for Bookkeeping Accounting and Financial
                 Reporting Powered by AI
               </p>
             </motion.div>
-            <motion.div
-              initial={{ x: -300, opacity: 0 }}
-              animate={{ opacity: 1, x: 0 }}
-              transition={{ duration: 3, delay: 2 }}
-              className="tracking-wide leading-relaxed font-bold font-monospace text-lg  text-white/70"
-            >
+            <motion.div  initial={{x:-300,opacity:0}} animate={{opacity:1,x:0}} transition={{duration:3,delay:2}}  className="tracking-wide  leading-relaxed font-bold font-monospace text-lg  text-white/70">
               Let f-ai help you manage your financial records and reporting
               requirements with our range of financial services.
             </motion.div>
@@ -33,12 +24,12 @@ const Description = () => {
           </div>
         </div>
         <div className="col-span-2 ">
-          <motion.div
-            initial={{ x: 300, opacity: 0 }}
-            animate={{ opacity: 1, x: 0 }}
-            transition={{ duration: 3, delay: 2 }}
-            className="flex flex-row justify-start items-center content-center h-full"
-          >
+          <motion.div  initial={{x:300,opacity:0}} animate={{opacity:1,x:0}} transition={{duration:3,delay:2}} className="hidden md:flex flex-row md:justify-start md:items-center md:content-center h-full">
+            <Button variant="mainButton" size="lg">
+              Learn More
+            </Button>
+          </motion.div>
+          <motion.div  initial={{x:-300,opacity:0}} animate={{opacity:1,x:"30vw"}} transition={{duration:3,delay:2}} className="md:hidden">
             <Button variant="mainButton" size="lg">
               Learn More
             </Button>
@@ -46,7 +37,7 @@ const Description = () => {
         </div>
       </main>
     </>
-  );
-};
+  )
+}
 
-export default Description;
+export default Description
